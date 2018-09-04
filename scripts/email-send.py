ecce9c4ebd2f52b2e91c71c6b69ed6a9e645ba01
Connor Mason <cnnr@icloud.com>
2018-09-03 19:38:16 -0500

    Trying to make fetch happen

--- scripts/email-send.py
+++ scripts/email-send.py
@@ -1,13 +1,21 @@
-<?php
-if(isset($_POST['submit'])){
-    $to = "connor@connote.design";
-    $from = $_POST['email'];
-    $name = $_POST['name'];
-    $subject = "Form submission";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+import smtplib
 
-    $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    echo "Thanks! Your email has been sent. We'll be in touch soon.";
-    }
-?>
+from email.mime.text import MIMEText
+
+# Open a plain text file for reading.  For this example, assume that
+# the text file contains only ASCII characters.
+with open(textfile, 'rb') as fp:
+    # Create a text/plain message
+    msg = MIMEText(fp.read())
+
+# me == the sender's email address
+# you == the recipient's email address
+msg['Subject'] = 'The contents of %s' % textfile
+msg['From'] = me
+msg['To'] = you
+
+# Send the message via our own SMTP server, but don't include the
+# envelope header.
+s = smtplib.SMTP('localhost')
+s.sendmail(me, [you], msg.as_string())
+s.quit()
