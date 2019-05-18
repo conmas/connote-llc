16b4f806ea0092d27ec5e12cd3871dbce47dc5d3
Billy Collins <billy.collins86@gmail.com>
2019-05-18 15:19:09 -0500

    form updates
    *Updated form button so it will submit and added button styles (input type buttons don't work)
    *On submit, change form to Thanks
    *If thanks, then user jquery to scroll the page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,9 @@
 // to do - add a script that scrolls the page to the form when there's an error
+
+$(document).ready(function(){
+	if($('.thanks').length) {
+		window.scroll(0,900);
+	} else {
+		console.log("Nothing to see here folks")
+	}
+});
--- routes/index.js
+++ routes/index.js
@@ -29,23 +29,26 @@ router.get('/contact', function(req, res, next) {
     res.render('contact', { title: 'Contact' });
 });
 
-router.post('/', [
-	check('first', 'Please enter normal characters only.').isAscii().trim(),
-	check('first', 'Please enter your first name.').isLength({min:1}).trim(),
-	check('last', 'Please enter normal characters only.').isAscii().trim(),
-	check('last', 'Please enter your last name.').isLength({min:1}).trim(),
-	check('email', 'Please enter an email address.').isLength({min:1}).trim(),
-	check('email', 'Please enter a valid email address.').isEmail().trim(),
-	check('message', 'Please enter a message.').isLength({min:1}).trim()
-	], function(req, res, next) {
+router.post('/', 
+	// [
+	// check('first', 'Please enter normal characters only.').isAscii().trim(),
+	// check('first', 'Please enter your first name.').isLength({min:1}).trim(),
+	// check('last', 'Please enter normal characters only.').isAscii().trim(),
+	// check('last', 'Please enter your last name.').isLength({min:1}).trim(),
+	// check('email', 'Please enter an email address.').isLength({min:1}).trim(),
+	// check('email', 'Please enter a valid email address.').isEmail().trim(),
+	// check('message', 'Please enter a message.').isLength({min:1}).trim()
+	// ], 
+	function(req, res, next) {
 
 		// deal with errors from the form validation
-		const errors = validationResult(req);
-  		if (!errors.isEmpty()) {
-    		return res.status(422).json({ errors: errors.array() });
-  		}
+		// const errors = validationResult(req);
+  // 		if (!errors.isEmpty()) {
+  //   		return res.status(422).json({ errors: errors.array() });
+  // 		}
 
   		// if no validation errors, then create the email
+		console.log('Data received!');
 		let data = {
 			from: req.body.email,
 			to: process.env.MAILGUN_TO_ADDRESS,
@@ -60,15 +63,15 @@ router.post('/', [
 				res.redirect('/');
 			} else {
 				console.log('An email from ' + req.body.email + ' was sent to ' + process.env.MAILGUN_TO_ADDRESS);
-				res.redirect('/thanks');
+				res.render('index', {title: 'Connote', element: 'thanks'});
 			}
 		});
 
 });
 
-router.get('/thanks', function(req, res, next) {
-	res.render('includes/thanks');
-})
+// router.get('/thanks', function(req, res, next) {
+// 	res.render('includes/thanks');
+// })
 
 
 module.exports = router;
